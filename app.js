02bcbbe6d8232f9b95f11af0363ca96022a1304b
Ernesto Parra <ernestoparra@MacBook-Pro-de-Ernesto.local>
2017-03-07 08:58:09 +0100

    adding styles homepage

---
+++ app.js
@@ -0,0 +1,25 @@
+const fs = require('fs')
+const request = require('request')
+const cheerio = require('cheerio')
+
+//---------------
+
+const amazonUrl ="https://www.amazon.es/s/ref=nb_sb_noss_2?__mk_es_ES=%C3%85M%C3%85%C5%BD%C3%95%C3%91&url=search-alias%3Daps&field-keywords=[KEYWORD]"
+
+
+request(amazonUrl, function (err, res, body){
+
+	var $ = cheerio.load(body);
+
+	var links = $('link').map(function(i) {
+			return $(this).attr('href') + "\n";
+    }).get();
+
+    fs.writeFile('test.txt', links, function(err){
+    	if (err) throw err;
+    	console.log('Its saved')
+    })
+
+
+})
+  
