1a5a29a3cb387c17516bc91bab7e24117940f388
Ernesto Parra <ernestoparra@MacBook-Pro-de-Ernesto.local>
2017-03-09 00:34:17 +0100

    adding styles

--- app.js
+++ app.js
@@ -6,8 +6,6 @@ const app = express()
 const PORT = process.env.PORT || 3000
 const bodyParser = require('body-parser')
 
-
-
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json())
