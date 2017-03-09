4e8c3465bf756095467e59cddeee3868b81df216
Ernesto Parra <ernestoparra@MacBook-Pro-de-Ernesto.local>
2017-03-09 09:32:28 +0100

    heroku changes

--- app.js
+++ app.js
@@ -6,13 +6,14 @@ const app = express()
 const PORT = process.env.PORT || 3000
 const bodyParser = require('body-parser')
 
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(__dirname + '/public'));
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json())
 
 
-app.get('/login', function (req, res){
-	res.redirect('home')
+app.get('/', function (req, res){
+	res.redirect('login')
 })
 
 var amazonImg = [];
