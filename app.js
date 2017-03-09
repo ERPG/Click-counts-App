2ed47b1cabcfe65a69330c31e42c16ae832dd1de
Ernesto Parra <ernestoparra@MacBook-Pro-de-Ernesto.local>
2017-03-09 16:50:15 +0100

    get info in JSON

--- app.js
+++ app.js
@@ -21,31 +21,78 @@ app.get('/login', function(req, res) {
 })
 
 var amazonImg = [];
+var aliexpressImg = [];
 
 app.post('/home/find', function(req, res) {
 
 
     const searchProduct = req.body.productSearch
 
-    const url = "https://www.amazon.es/s/ref=nb_sb_noss_2?__mk_es_ES=%C3%85M%C3%85%C5%BD%C3%95%C3%91&url=search-alias%3Daps&field-keywords=[KEYWORD]"
+    const url = "https://www.amazon.es/s/?field-keywords=[KEYWORD]"
 
     const amazonUrl = url.replace('[KEYWORD]', searchProduct)
 
-    request(amazonUrl, (err, res, body) => {
+    // request(amazonUrl, (err, res, body) => {
 
-        var $ = cheerio.load(body);
 
-        $('ul#s-results-list-atf img.s-access-image.cfMarker').each(function(i, elem) {
+    //     var amazon = []
 
-            var images = $(this).attr('src')
 
-            amazonImg.push(images)
-        });
+    //     var $ = cheerio.load(body);
 
-        console.log(amazonImg);
+    //     $('ul#s-results-list-atf li').each(function(i, elem) {
+    //         let product = {}
+    //         product.images = $(this).find('img.s-access-image.cfMarker').attr('src')
+    //         product.price = $(this).find('span.a-size-base.a-color-price.s-price.a-text-bold').text()
+    //         product.description = $(this).find('h2.a-size-medium.a-color-null.s-inline.scx-truncate.s-access-title.color-variation-title-replacement.a-text-normal').text()
+
+    //         console.log(product);
+
+    //         amazon.push(product)
+
+    //         var jsonAmazon = JSON.stringify(amazon);
+
+    //         fs.writeFile('data/data.json', jsonAmazon, 'utf8', function(err) {
+    //             console.log('saved products');
+
+    //         })
+    //     })
+
+    // })
+
+    const urlali = "https://es.aliexpress.com/wholesale?catId=0&initiative_id=[products-ID]&SearchText=[KEYWORD]"
+
+    const aliUrl = urlali.replace('[KEYWORD]', searchProduct)
+
+    request(aliUrl, (err, res, body) => {
+
+    	var $ = cheerio.load(body);
+
+        var aliexpress = []
+
+        $('ul#hs-list-items li').each(function(i, elem) {
+            let product = {}
+            product.images = $(this).find('img.picCore.pic-Core-v').attr('src')
+            product.price = $(this).find('span.value').text()
+            product.description = $(this).find('a.history-item.product').text()
+
+            console.log(product);
+
+            aliexpress.push(product)
+
+            var jsonAliexpress = JSON.stringify(aliexpress);
+
+            fs.writeFile('data/data.json', jsonAliexpress, 'utf8', function(err) {
+                console.log('saved products');
+
+            })
+        })
+
     })
 
-    res.render('home.html')
 })
+
+// -------------------------- aliexpress------------------------------
+
 
 app.listen(PORT, () => console.log(`listening on PORT ${ PORT }...`))
