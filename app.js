ad9230a185160ec99d34f0832bd1cf5c2adcaf7f
Ernesto Parra <ernestoparra@MacBook-Pro-de-Ernesto.local>
2017-03-13 10:43:53 +0100

    searching results with angular

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const app = express()
 const PORT = process.env.PORT || 3000
 const bodyParser = require('body-parser')
 
+var amazonProducts = []
+
+
 
 app.use(express.static(__dirname + '/public'));
 
@@ -16,34 +19,26 @@ app.use(bodyParser.json())
 app.post('/api/search', function(req, res) {
 
 
-    const searchProduct = req.body.productSearch
+	const searchProduct = req.body.productSearch
 
-    const url = "https://www.amazon.es/s/?field-keywords=[KEYWORD]"
+	const url = "https://www.amazon.es/s/?field-keywords=[KEYWORD]"
 
-    const amazonUrl = url.replace('[KEYWORD]', searchProduct)
+	const amazonUrl = url.replace('[KEYWORD]', searchProduct)
 
-    request(amazonUrl, (err, res, body) => {
+	request(amazonUrl, (err, response, body) => {
 
+		var $ = cheerio.load(body);
 
-        var amazon = []
+		$('ul#s-results-list-atf li').each(function(i, elem) {
+			let product = {}
+			product.images = $(this).find('img.s-access-image.cfMarker').attr('src')
+			product.price = $(this).find('span.a-size-base.a-color-price.s-price.a-text-bold').text()
+			product.description = $(this).find('h2.a-size-medium.a-color-null.s-inline.scx-truncate.s-access-title.color-variation-title-replacement.a-text-normal').text()
+			amazonProducts.push(product)
+		})
 
-
-        var $ = cheerio.load(body);
-
-        $('ul#s-results-list-atf li').each(function(i, elem) {
-            let product = {}
-            product.images = $(this).find('img.s-access-image.cfMarker').attr('src')
-            product.price = $(this).find('span.a-size-base.a-color-price.s-price.a-text-bold').text()
-            product.description = $(this).find('h2.a-size-medium.a-color-null.s-inline.scx-truncate.s-access-title.color-variation-title-replacement.a-text-normal').text()
-
-            console.log(product);
-
-        })
-
-        res.json(product)
-
-    })
-
+		res.json(amazonProducts)
+	})
 
 })
 
--- public/js/main.js
+++ public/js/main.js
@@ -1,37 +1,46 @@
 angular.module('Click-counts-app', ['ngRoute'])
-	.config( function ($routeProvider){
+    .config(function($routeProvider) {
 
-		$routeProvider
-			.when('/', {
-				templateUrl: '/login.html',
-				controller: 'loginController'
-			})
-			.when('/home', {
-				templateUrl: '/home.html',
-				controller: 'homeController'
-			})
-			.otherwise('/')
-	})
+        $routeProvider
+            .when('/login', {
+                templateUrl: '/login.html',
+                controller: 'loginController'
+            })
+            .when('/', {
+                templateUrl: '/home.html',
+                controller: 'homeController'
+            })
+            .otherwise('/')
+    })
 
-.controller('homeController', function($scope, $rootScope) {
+.controller('homeController', function($scope, $rootScope, DataFactory) {
 
-	$rootScope.SectionHome = true
+    $rootScope.SectionHome = true
+
+    $scope.getSearch = function(e) {
+        e.preventDefault()
+        $scope.SectionSearch = true
+        DataFactory.getSearch($scope.SearchProduct)
+    }
 })
-
-
 
 .controller('loginController', function($scope, $rootScope) {
 
-	$scope.SectionLogin = true
+    $scope.SectionLogin = true
 
 })
 
 
-.factory('DataFactory', function($http) {})
+.factory('DataFactory', function($http, $rootScope) {
 
-function getSearch() {
-    return $http.get('/api/search')
-        .then(({ data }) => data)
-        .then(console.log())
-}
+    function getSearch() {
+        return $http.post('/api/search')
+            .then((  { data } ) => data )
+            .then( products => $rootScope.products = products)
+    }
+    return {
+    getSearch: getSearch
 
+    } 
+
+})
