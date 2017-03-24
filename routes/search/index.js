2ec922d4527624fabd534991296739d3508a3cd7
Ernesto Parra <ernestoparra@MacBook-Pro-de-Ernesto.local>
2017-03-24 16:53:19 +0100

    fixing heroku errors

--- routes/search/index.js
+++ routes/search/index.js
@@ -8,11 +8,11 @@ const express = require('express')
 const router = express.Router()
 const EBAY_API_KEY = process.env.EBAY_API_KEY
 
-const url = `https://svcs.ebay.com/services/search/FindingService/v1?SECURITY-APPNAME=${EBAY_API_KEY}&OPERATION-NAME=findItemsByKeywords&SERVICE-VERSION=1.0.0&RESPONSE-DATA-FORMAT=JSON&callback=_cb_findItemsByKeywords&REST-PAYLOAD&keywords=[KEYWORD]&paginationInput.entriesPerPage=6&GLOBAL-ID=EBAY-ES&siteid=186`
-const urlCarref = 'https://www.carrefour.es/global/?Dy=1&Nty=1&Ntt=[KEYWORD]&search='
-const urlCI = 'https://www.elcorteingles.es/search/?s=[KEYWORD]'
-const urlFnac = 'https://busqueda.fnac.es/SearchResult/ResultList.aspx?SCat=0%211&Search=[KEYWORD]&sft=1&sa=0'
-const urlSolo = 'https://www.solostocks.com/venta-productos/[KEYWORD]_b'
+const url = `http://svcs.ebay.com/services/search/FindingService/v1?SECURITY-APPNAME=${EBAY_API_KEY}&OPERATION-NAME=findItemsByKeywords&SERVICE-VERSION=1.0.0&RESPONSE-DATA-FORMAT=JSON&callback=_cb_findItemsByKeywords&REST-PAYLOAD&keywords=[KEYWORD]&paginationInput.entriesPerPage=6&GLOBAL-ID=EBAY-ES&siteid=186`
+const urlCarref = 'http://www.carrefour.es/global/?Dy=1&Nty=1&Ntt=[KEYWORD]&search='
+const urlCI = 'http://www.elcorteingles.es/search/?s=[KEYWORD]'
+const urlFnac = 'http://busqueda.fnac.es/SearchResult/ResultList.aspx?SCat=0%211&Search=[KEYWORD]&sft=1&sa=0'
+const urlSolo = 'http://www.solostocks.com/venta-productos/[KEYWORD]_b'
 
 router.post('/search', function(req, res) {
 
@@ -72,8 +72,6 @@ router.post('/search', function(req, res) {
 
         let carrefFiltered = carref.filter( filterOthersPriceOverAverage )
                                     .filter( justThree )
-
-        console.log(carrefFiltered)
 
         let ebayFiltered = ebay.filter( filterEBayPriceOverAverage )
                                 .filter( justThree )
