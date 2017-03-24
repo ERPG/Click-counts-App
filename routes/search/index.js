2d5b10b4101a9266cd37ab21c3b375220b204af8
Ernesto Parra <ernestoparra@MacBook-Pro-de-Ernesto.local>
2017-03-24 13:50:18 +0100

    fixing

--- routes/search/index.js
+++ routes/search/index.js
@@ -6,8 +6,9 @@ const getPromiseSolo = require('./providers/soloStock')
 
 const express = require('express')
 const router = express.Router()
+const EBAY_API_KEY = process.env.EBAY_API_KEY
 
-const url = 'https://svcs.ebay.com/services/search/FindingService/v1?SECURITY-APPNAME=ErnestoP-clickcou-PRD-e6c385072-30fa6fdb&OPERATION-NAME=findItemsByKeywords&SERVICE-VERSION=1.0.0&RESPONSE-DATA-FORMAT=JSON&callback=_cb_findItemsByKeywords&REST-PAYLOAD&keywords=[KEYWORD]&paginationInput.entriesPerPage=6&GLOBAL-ID=EBAY-ES&siteid=186'
+const url = `https://svcs.ebay.com/services/search/FindingService/v1?SECURITY-APPNAME=${EBAY_API_KEY}&OPERATION-NAME=findItemsByKeywords&SERVICE-VERSION=1.0.0&RESPONSE-DATA-FORMAT=JSON&callback=_cb_findItemsByKeywords&REST-PAYLOAD&keywords=[KEYWORD]&paginationInput.entriesPerPage=6&GLOBAL-ID=EBAY-ES&siteid=186`
 const urlCarref = 'http://www.carrefour.es/global/?Dy=1&Nty=1&Ntt=[KEYWORD]&search='
 const urlCI = 'https://www.elcorteingles.es/search/?s=[KEYWORD]'
 const urlFnac = 'http://busqueda.fnac.es/SearchResult/ResultList.aspx?SCat=0%211&Search=[KEYWORD]&sft=1&sa=0'
